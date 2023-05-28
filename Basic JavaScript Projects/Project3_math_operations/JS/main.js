abfd8fbb222cb1193f5894b456a04f4b5e0dd5bd
bsyms855 <127457570+bsyms855@users.noreply.github.com>
2023-05-28 10:23:50 -0700

    Math methods

--- Basic JavaScript Projects/Project3_math_operations/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operations/JS/main.js
@@ -1,20 +1,20 @@
 function plusMethod(){
     var plusMethod = 2 + 3;
-    document.getElementById("Math").innerHTML = "2 + 3 = " + plusMethod;
+    document.getElementById("Math").innerHTML = "2 plus 3 = " + plusMethod;
 }
 function minusMethod(){
     var minusMethod = 50 - 25;
-    document.getElementById("Math").innerHTML = "50 - 25 = " - minusMethod;
+    document.getElementById("Math").innerHTML = "50 minus 25 = " + minusMethod;
 }
 function timesMethod(){
     var timesMethod = 5 * 5;
-    document.getElementById("Math").innerHTML = "5 * 5 = " * timesMethod; 
+    document.getElementById("Math").innerHTML = "5 times 5 = " + timesMethod; 
 }
 function divideMethod(){
     var divideMethod = 5 % 1;
-    document.getElementById("Math").innerHTML = "5 % 1 = " % divideMethod;
+    document.getElementById("Math").innerHTML = "5 divided by 1 = " + divideMethod;
 }
 function MathRandom(){
     var MathRandom = 5 + 1 = 6 - 2 = 9 + 4 = 5 - 5 =;
-    document.getElementById("MathRandom").innerHTML = "5 + 1 = 6 - 2 = 9 + 4 = 5 - 5 = " MathRandom;
+    document.getElementById("MathRandom").innerHTML = "5 plus 1 equals 6 minus 2 equals 9 plus 4 equals 5 minus 5 equals " + MathRandom;
 }
