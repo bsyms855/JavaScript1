1537fa9fffbe24cb46b267869a66637a7bfc87db
bsyms855 <127457570+bsyms855@users.noreply.github.com>
2023-06-05 10:13:17 -0700

    dictionary

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,11 @@
+let petDictionary = {
+    "Orion": { Age: 2, Color: "Calico", Gender: "Male" },
+    "Twila": { Age: 4, Color: "Black", Gender: "Female" },
+};
+
+const obj = { a: 1 };
+Object.assign(obj, { b: 2 }, { c: 3 });
+// obj = { a: 1, b: 2, c: 3 }
+
+function
+document.getElementById("Dictionary").innerHTML
